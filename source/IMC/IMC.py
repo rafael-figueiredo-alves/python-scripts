6ac008f46c1ee286252f5f5e8b0945985a9a577a
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2024-09-02 19:05:49 -0300

    Improving functions

--- source/IMC/IMC.py
+++ source/IMC/IMC.py
@@ -5,27 +5,18 @@ MyFunctions.CleanConsole()
 print('########################################################');
 print('                 CALCULADORA DE IMC                     ');
 print('########################################################');
+
 print('');
-pesostr = input('Qual é o seu peso?\n');
-peso = float(pesostr.replace(',', '.'));
+peso = MyFunctions.InputFloat('Qual é o seu peso?');
+
 print('');
-alturastr = input('Qual é a sua altura?\n');
-altura = float(alturastr.replace(',', '.'));
+altura = MyFunctions.InputFloat('Qual é a sua altura?');
+
 IMC = peso / (altura * altura);
 print('');
 Resultado = '';
-if(IMC < 18.5):
-   Resultado = 'abaixo do peso, esqueletinho!';
-elif (IMC >= 18.5 and IMC <= 24.9):
-   Resultado = 'com peso adequado, miserável!';
-elif (IMC >= 25 and IMC <= 29.9):
-  Resultado = 'sobrepeso, um regiminho ajuda, hein!';
-elif (IMC >= 30 and IMC <= 34.9):
-  Resultado = 'obeso, sinal amarelo!';
-elif (IMC >= 35 and IMC <= 29.9):
-  Resultado = 'obeso, sinal vermelho, fecha essa boca, OK?'
-else:
-  Resultado = 'uma baleia com obesidade grau 3, fecha essa boca pelo amor de Deus!';
+
+Resultado = MyFunctions.ShowResult(IMC);
 
 print('Você está ', Resultado);
 
--- source/IMC/MyFunctions.py
+++ source/IMC/MyFunctions.py
@@ -2,3 +2,24 @@ import os
 
 def CleanConsole():
 	os.system('cls' if os.name == 'nt' else 'clear')
+
+def InputFloat(PromptText):
+	valorStr = input(PromptText + '\n');
+	valor = float(valorStr.replace(',', '.'));
+	return valor;
+
+def ShowResult(IMC):
+        Resultado = '';
+	if(IMC < 18.5):
+   	  Resultado = 'abaixo do peso, esqueletinho!';
+	elif (IMC >= 18.5 and IMC <= 24.9):
+	  Resultado = 'com peso adequado, miserável!';
+	elif (IMC >= 25 and IMC <= 29.9):
+  	  Resultado = 'sobrepeso, um regiminho ajuda, hein!';
+	elif (IMC >= 30 and IMC <= 34.9):
+	  Resultado = 'obeso, sinal amarelo!';
+	elif (IMC >= 35 and IMC <= 29.9):
+  	  Resultado = 'obeso, sinal vermelho, fecha essa boca, OK?'
+	else:
+  	  Resultado = 'uma baleia com obesidade grau 3, fecha essa boca pelo amor de Deus!';
+	return Resultado;
