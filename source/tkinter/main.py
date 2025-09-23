c1d7b08187b98e259367a480d4696819d4a6ac40
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2025-09-23 18:51:03 -0300

    Update main.py

--- source/tkinter/main.py
+++ source/tkinter/main.py
@@ -1,16 +1,30 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def Button_click():
+numero = 0
+
+def btnCounter():
+    global numero
+    numero += 1
+    labelCount.config(text=f"Valor de número: {numero}")
+
+def ShowMsg():
     messagebox.showwarning("ATENÇÃO", "Você pressionou o botão!")
 
 root = tk.Tk()
 root.title("Hello world from Python Tkinter")
+root.geometry("400x200")
 
-label = tk.Label(root, text="Teste")
+label = tk.Label(root, text="Clique no botão para incrementar")
 label.pack()
 
-button = tk.Button(root, text="Ok", command=Button_click)
+button = tk.Button(root, text="Incrementar", command=btnCounter)
 button.pack()
+
+labelCount = tk.Label(root, text=f"Valor de número: {numero}")
+labelCount.pack()
+
+buttonMsg = tk.Button(root, text="Mostrar Msg", command=ShowMsg)
+buttonMsg.pack()
 
 root.mainloop()
