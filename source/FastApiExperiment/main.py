2ffd9272c31532a32e606af346e4eb3a4607123c
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2025-11-02 16:16:12 -0300

    Practicing Comments on Python

--- source/FastApiExperiment/main.py
+++ source/FastApiExperiment/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Este comando vai exibir "Hello, World!" na raiz da aplicação
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
 
+#Este endpoint recebe um nome como parâmetro e retorna uma saudação personalizada
 @app.get("/{name}")
 async def greet(name: str):
     return {"message": f"Hello, {name}!"}
+
+"""
+Este é um exemplo simples de uma aplicação FastAPI com dois endpoints:
+1. O endpoint raiz ("/") que retorna uma mensagem "Hello, World!".
+2. O endpoint "/{name}" que retorna uma saudação personalizada com o nome fornecido
+    na URL.
+Versão 0.1
+"""
