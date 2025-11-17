5e4117f80dee179f9d6f47235d47758e8296acbe
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2025-11-17 18:47:40 -0300

    Update exerciseVariables.py

--- source/Exercises/exerciseVariables.py
+++ source/Exercises/exerciseVariables.py
@@ -4,3 +4,28 @@ y = "Hello"
 print("The value of x is:", x)
 print("The value of y is:", y)
 print("Este é um exemplo de exercício sobre variáveis.")
+
+num = int(50)
+palavra = str("Ola mundo das variáveis: ")
+dinheiro = float(3.14)
+
+print(palavra + str(num) + " e o valor de pi é aproximadamente " + str(dinheiro))
+
+p = 5
+
+print("A variável p é do tipo:", type(p))
+
+num1, num2, num3 = 1, 2, 3
+
+print("Os valores das variáveis são:", num1, num2, num3)
+
+num4 = num5 = num6 = 100
+
+print("Os valores das variáveis num4, num5 e num6 são:", num4, num5, num6)
+
+valor1, valor2 = ["ola", 42]
+
+print("Os valores atribuídos são:", valor1, "e", valor2)
+
+valor3, valor4 = (3.5, "mundo")
+print("Os valores atribuídos são:", valor3, "e", valor4)
